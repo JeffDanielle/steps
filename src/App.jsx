56f3ac8b75dc6fb5532208dff8af29a37f2e00a3
JeffDanielle <bayotjeff@gmail.com>
2024-03-18 12:40:41 +0800

    57. Let's Build a Steps Component -15min

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
+import './custom.css'
+import Msg from "./data/messages"
 
-function App() {
+const App = () => {
+  const step = 1;
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="steps">
+      <div className='numbers'>
+        <div className={`${step >= 1 ? 'active' : ''}`}> 1 </div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>  2 </div>
+        <div className={`${step >= 3 ? 'active' : ''}`}> 3 </div>
+      </div>
+      <p className="message">Step {step}: {Msg[step - 1]}</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>Previous</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>Next</button>
+      </div>
+    </div>
   )
 }
 
